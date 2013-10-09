54471722a0bb23aae0f3faa8169e71d43fbb9142
lrzuniga <lrzuniga@gmail.com>
2013-10-09 12:35:52 -0400

    modified method to match course to resources tagged with units

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,7 +3,7 @@ class Course < ActiveRecord::Base
   acts_as_taggable_on :units
 
   def resources_tagged_with_units
-    Resource.tagged_with(self.unit_list)
+    Resource.tagged_with(self.unit_list, :any => true)
   end
 
 end
